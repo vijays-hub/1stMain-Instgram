d6a54099d79309a151bfd4a3056ccb829db1ab9e
Vijay S Kumar <63061945+vijays-hub@users.noreply.github.com>
2020-12-16 16:44:35 +0530

    Update Post.jsx

--- src/Posts/Post.jsx
+++ src/Posts/Post.jsx
@@ -4,7 +4,7 @@ import PostImage from "./PostImage";
 const Post = ({ urls }) => {
   return (
     <div className="row mt-5">
-      <div className="row d-flex .flex-sm-wrap .flex-md-wrap justify-content-between">
+      <div className="row d-flex justify-content-between">
         {urls.map((url) => {
           return (
             <div className="col-sm-4" key="url">
